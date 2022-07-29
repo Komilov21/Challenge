eeb6bc57b04af8665df8c1474dd6150b687f4fbe
Komilov Khayrulla <108469321+Komilov21@users.noreply.github.com>
2022-07-29 15:29:02 +0200

    html and css part

---
+++ index.js
@@ -0,0 +1,20 @@
+let teamNumber = 0;
+
+
+
+let nameAdd = function(){
+    const addNameToList = document.getElementById("name")
+    const listItems = document.getElementsByClassName("listItems")
+    addNameToList.innerText.push(listItems)
+}
+nameAdd()
+
+
+
+
+
+
+
+
+
+
